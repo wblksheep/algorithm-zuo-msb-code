7189c8d6909ced193ba3d9e412995b77af553c7a
algorithmzuo <algorithmzuo@163.com>
2023-03-02 13:58:26 +0800

    modify code

---
+++ MCA算法突击课/第03期/mca_04/Code01_CompleteTreeNodeNumber.java
@@ -0,0 +1,50 @@
+package 第03期.mca_04;
+
+// 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数
+// 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外
+// 其余每层节点数都达到最大值
+// 并且最下面一层的节点都集中在该层最左边的若干位置
+// 若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+// 测试链接 : https://leetcode.cn/problems/count-complete-tree-nodes/
+public class Code01_CompleteTreeNodeNumber {
+
+	// 提交时不要提交这个类
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+	}
+
+	// 提交如下的方法
+	public static int countNodes(TreeNode head) {
+		if (head == null) {
+			return 0;
+		}
+		return bs(head, 1, mostLeftLevel(head, 1));
+	}
+
+	// 当前来到node节点，node节点在level层，总层数是h
+	// 返回node为头的子树(必是完全二叉树)，有多少个节点
+	public static int bs(TreeNode node, int Level, int h) {
+		if (Level == h) {
+			return 1;
+		}
+		if (mostLeftLevel(node.right, Level + 1) == h) {
+			return (1 << (h - Level)) + bs(node.right, Level + 1, h);
+		} else {
+			return (1 << (h - Level - 1)) + bs(node.left, Level + 1, h);
+		}
+	}
+
+	// 如果node在第level层，
+	// 求以node为头的子树，最大深度是多少
+	// node为头的子树，一定是完全二叉树
+	public static int mostLeftLevel(TreeNode node, int level) {
+		while (node != null) {
+			level++;
+			node = node.left;
+		}
+		return level - 1;
+	}
+
+}
---
+++ MCA算法突击课/第03期/mca_04/Code05_FriendCircles.java
@@ -0,0 +1,70 @@
+package 第03期.mca_04;
+
+// 本题为leetcode原题
+// 测试链接：https://leetcode.cn/problems/friend-circles/
+// 可以直接通过
+public class Code05_FriendCircles {
+
+	public static int findCircleNum(int[][] M) {
+		int N = M.length;
+		UnionFind uf = new UnionFind(N);
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (M[i][j] == 1) { // i和j互相认识
+					uf.union(i, j);
+				}
+			}
+		}
+		return uf.sets();
+	}
+
+	public static class UnionFind {
+		private int[] father;
+		private int[] size;
+		private int[] help;
+		private int sets;
+
+		public UnionFind(int N) {
+			father = new int[N];
+			size = new int[N];
+			help = new int[N];
+			sets = N;
+			for (int i = 0; i < N; i++) {
+				father[i] = i;
+				size[i] = 1;
+			}
+		}
+
+		private int find(int i) {
+			int hi = 0;
+			while (i != father[i]) {
+				help[hi++] = i;
+				i = father[i];
+			}
+			for (hi--; hi >= 0; hi--) {
+				father[help[hi]] = i;
+			}
+			return i;
+		}
+
+		public void union(int i, int j) {
+			int f1 = find(i);
+			int f2 = find(j);
+			if (f1 != f2) {
+				if (size[f1] >= size[f2]) {
+					size[f1] += size[f2];
+					father[f2] = f1;
+				} else {
+					size[f2] += size[f1];
+					father[f1] = f2;
+				}
+				sets--;
+			}
+		}
+
+		public int sets() {
+			return sets;
+		}
+	}
+
+}
