5ca934ef7d8c8da3e5b1f8290b90d92eb4254fe7
algorithmzuo <algorithmzuo@163.com>
2023-03-02 19:18:05 +0800

    modify code

--- MCA算法突击课/第03期/mca_04/Code05_CouplesHoldingHands.java
+++ MCA算法突击课/第03期/mca_04/Code05_CouplesHoldingHands.java
@@ -6,14 +6,10 @@ package 第03期.mca_04;
 // 返回 最少交换座位的次数，以便每对情侣可以并肩坐在一起
 // 每次交换可选择任意两人，让他们站起来交换座位
 // 测试链接 : https://leetcode.cn/problems/couples-holding-hands/
-public class Code06_CouplesHoldingHands {
+public class Code05_CouplesHoldingHands {
 
 	public int minSwapsCouples(int[] row) {
-		// n人数，偶数
 		int n = row.length;
-		// n/2 
-		// 0 1 -> 0 0
-		// 4 5 -> 2 2
 		UnionFind uf = new UnionFind(n / 2);
 		for (int i = 0; i < n; i += 2) {
 			uf.union(row[i] / 2, row[i + 1] / 2);
