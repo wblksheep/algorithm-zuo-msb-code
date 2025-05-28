6e4917a10d1a017301affc9a4ea159f7a97b5683
wblksheep <3197217952@qq.com>
2025-05-28 20:31:29 +0800

    my commit

--- 大厂刷题班/class18/Code03_CherryPickup.java
+++ 大厂刷题班/class18/Code03_CherryPickup.java
@@ -16,28 +16,26 @@ import java.io.StreamTokenizer;
 
 public class Code03_CherryPickup {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StreamTokenizer in = new StreamTokenizer(br);
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-        while (in.nextToken() != StreamTokenizer.TT_EOF) {
-            int N = (int) in.nval;
-            in.nextToken();
-            int M = (int) in.nval;
-            int[][] matrix = new int[N][M];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    in.nextToken();
-                    matrix[i][j] = (int) in.nval;
-                }
-            }
-            out.println(cherryPickup(matrix));
-            out.flush();
-        }
-    }
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StreamTokenizer in = new StreamTokenizer(br);
+//        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+//        while (in.nextToken() != StreamTokenizer.TT_EOF) {
+//            int N = (int) in.nval;
+//            in.nextToken();
+//            int M = (int) in.nval;
+//            int[][] matrix = new int[N][M];
+//            for (int i = 0; i < N; i++) {
+//                for (int j = 0; j < M; j++) {
+//                    in.nextToken();
+//                    matrix[i][j] = (int) in.nval;
+//                }
+//            }
+//            out.println(cherryPickup(matrix));
+//            out.flush();
+//        }
+//    }
 
-    // 如下方法，在leetcode上提交也能通过
-    // 测试链接 : https://leetcode.cn/problems/cherry-pickup/
     public static int cherryPickup(int[][] grid) {
         int N = grid.length;
         int M = grid[0].length;
@@ -64,12 +62,16 @@ public class Code03_CherryPickup {
             dp[x1][y1][x2] = grid[x1][y1];
             return dp[x1][y1][x2];
         }
+        if (grid[x1][y1] == -1 || grid[x2][x1 + y1 - x2] == -1) {
+            dp[x1][y1][x2] = -1;
+            return dp[x1][y1][x2];
+        }
         int next = Integer.MIN_VALUE;
         next = Math.max(next, process(grid, x1 + 1, y1, x2 + 1, dp));
         next = Math.max(next, process(grid, x1 + 1, y1, x2, dp));
         next = Math.max(next, process(grid, x1, y1 + 1, x2, dp));
         next = Math.max(next, process(grid, x1, y1 + 1, x2 + 1, dp));
-        if (grid[x1][y1] == -1 || grid[x2][x1 + y1 - x2] == -1 || next == -1) {
+        if (next == -1) {
             dp[x1][y1][x2] = -1;
             return dp[x1][y1][x2];
         }
@@ -79,6 +81,62 @@ public class Code03_CherryPickup {
         }
         dp[x1][y1][x2] = grid[x1][y1] + grid[x2][x1 + y1 - x2] + next;
         return dp[x1][y1][x2];
+    }
+
+    public static int cherryPickup2(int[][] grid) {
+        int N = grid.length;
+        int M = grid[0].length;
+        int[][][] dp = new int[N][M][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                for (int k = 0; k < N; k++) {
+                    dp[i][j][k] = Integer.MIN_VALUE;
+                }
+            }
+        }
+        int ans = p(grid, 0, 0, 0, dp);
+        return ans < 0 ? 0 : ans;
+    }
+
+    public static int p(int[][] grid, int x1, int y1, int x2, int[][][] dp) {
+        if (x1 == grid.length || y1 == grid[0].length || x2 == grid.length || x1 + y1 - x2 == grid[0].length) {
+            return Integer.MIN_VALUE;
+        }
+        if (dp[x1][y1][x2] != Integer.MIN_VALUE) {
+            return dp[x1][y1][x2];
+        }
+        if (x1 == grid.length - 1 && y1 == grid[0].length - 1) {
+            dp[x1][y1][x2] = grid[x1][y1];
+            return dp[x1][y1][x2];
+        }
+        if (grid[x1][y1] == -1 || grid[x2][x1 + y1 - x2] == -1) {
+            dp[x1][y1][x2] = -1;
+            return dp[x1][y1][x2];
+        }
+        int p1 = p(grid, x1 + 1, y1, x2 + 1, dp);
+        int p2 = p(grid, x1 + 1, y1, x2, dp);
+        int p3 = p(grid, x1, y1 + 1, x2 + 1, dp);
+        int p4 = p(grid, x1, y1 + 1, x2, dp);
+        int next = Math.max(p1, Math.max(p2, Math.max(p3, p4)));
+        if (next == -1) {
+            dp[x1][y1][x2] = -1;
+            return dp[x1][y1][x2];
+        }
+        if (x1 == x2) {
+            dp[x1][y1][x2] = next + grid[x1][y1];
+            return dp[x1][y1][x2];
+        }
+        dp[x1][y1][x2] = grid[x1][y1] + grid[x2][x1 + y1 - x2] + next;
+        return dp[x1][y1][x2];
+    }
+
+    public static void main(String[] args) {
+//        int[][] m = {{0, 1, -1}, {1, 0, -1}, {1, 1, 1}};
+//        int[][] m = {{0}};
+//        int[][] m = {{1, 1, -1}, {1, -1, 1}, {-1, 1, 1}};
+        int[][] m = {{1, 1, 1, 1, -1, -1, -1, 1, 0, 0}, {1, 0, 0, 0, 1, 0, 0, 0, 1, 0}, {0, 0, 1, 1, 1, 1, 0, 1, 1, 1}, {1, 1, 0, 1, 1, 1, 0, -1, 1, 1}, {0, 0, 0, 0, 1, -1, 0, 0, 1, -1}, {1, 0, 1, 1, 1, 0, 0, -1, 1, 0}, {1, 1, 0, 1, 0, 0, 1, 0, 1, -1}, {1, -1, 0, 1, 0, 0, 0, 1, -1, 1}, {1, 0, -1, 0, -1, 0, 0, 1, 0, 0}, {0, 0, -1, 0, 1, 0, 1, 0, 0, 1}};
+        System.out.println(cherryPickup(m));
+        System.out.println(cherryPickup2(m));
     }
 
 }
