08776d5a6590168ef1dfbef61b1420417103823e
wblksheep <3197217952@qq.com>
2025-06-16 10:01:15 +0800

    my commit

--- 大厂刷题班/class23/Code04_FindKMajorityEdition1.java
+++ 大厂刷题班/class23/Code04_FindKMajorityEdition1.java
@@ -1,5 +1,7 @@
 package class23;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -36,67 +38,51 @@ public class Code04_FindKMajorityEdition1 {
     }
 
     public static void printKMajor(int[] arr, int K) {
-        if (K < 2) {
-            System.out.println("the value of K is invalid.");
-            return;
-        }
-        // 攒候选，cands，候选表，最多K-1条记录！ > N / K次的数字，最多有K-1个
-        HashMap<Integer, Integer> cands = new HashMap<Integer, Integer>();
-        for (int i = 0; i != arr.length; i++) {
-            if (cands.containsKey(arr[i])) {
-                cands.put(arr[i], cands.get(arr[i]) + 1);
-            } else { // arr[i] 不是候选
-                if (cands.size() == K - 1) { // 当前数肯定不要！，每一个候选付出1点血量，血量变成0的候选，要删掉！
-                    allCandsMinusOne(cands);
-                } else {
-                    cands.put(arr[i], 1);
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else if (map.size() == K - 1) {
+                allCandsMinusOne(map);
+            } else {
+                map.put(arr[i], 1);
             }
         }
-        // 所有可能的候选，都在cands表中！遍历一遍arr，每个候选收集真实次数
-
-
-        HashMap<Integer, Integer> reals = getReals(arr, cands);
+        HashMap<Integer, Integer> reals = getReal(arr, map);
         boolean hasPrint = false;
-        for (Entry<Integer, Integer> set : cands.entrySet()) {
-            Integer key = set.getKey();
-            if (reals.get(key) > arr.length / K) {
+        for (Entry<Integer, Integer> entry : reals.entrySet()) {
+            if (entry.getValue() > arr.length / K) {
+                System.out.print(entry.getKey() + " ");
                 hasPrint = true;
-                System.out.print(key + " ");
             }
         }
-        System.out.println(hasPrint ? "" : "no such number.");
+        System.out.println(hasPrint?"":"No such major");
+    }
+
+    public static HashMap<Integer, Integer> getReal(int[] arr, HashMap<Integer, Integer> cands) {
+        HashMap<Integer, Integer> ans = new HashMap<>();
+        for (Integer i : cands.keySet()) {
+            ans.put(i, 0);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (ans.containsKey(arr[i])) {
+                ans.put(arr[i], ans.get(arr[i]) + 1);
+            }
+        }
+        return ans;
     }
 
     public static void allCandsMinusOne(HashMap<Integer, Integer> map) {
-        List<Integer> removeList = new LinkedList<Integer>();
-        for (Entry<Integer, Integer> set : map.entrySet()) {
-            Integer key = set.getKey();
-            Integer value = set.getValue();
-            if (value == 1) {
-                removeList.add(key);
+        List<Integer> removeList = new LinkedList<>();
+        for (Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                removeList.add(entry.getKey());
             }
-            map.put(key, value - 1);
+            map.put(entry.getKey(), map.get(entry.getKey()) - 1);
         }
-        for (Integer removeKey : removeList) {
-            map.remove(removeKey);
+        for (Integer i : removeList) {
+            map.remove(i);
         }
-    }
-
-    public static HashMap<Integer, Integer> getReals(int[] arr,
-                                                     HashMap<Integer, Integer> cands) {
-        HashMap<Integer, Integer> reals = new HashMap<Integer, Integer>();
-        for (int i = 0; i != arr.length; i++) {
-            int curNum = arr[i];
-            if (cands.containsKey(curNum)) {
-                if (reals.containsKey(curNum)) {
-                    reals.put(curNum, reals.get(curNum) + 1);
-                } else {
-                    reals.put(curNum, 1);
-                }
-            }
-        }
-        return reals;
     }
 
     public static void main(String[] args) {
